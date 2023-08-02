4eb2a826e96cb5351378162cb2bf2c4994f50a47
jelders25 <jack.elders@contentsquare.com>
2023-08-02 10:15:36 -0700

    a

--- js/scripts.js
+++ js/scripts.js
@@ -46,9 +46,9 @@ buttonGet.addEventListener('click', () => {
 
 //API Error GraphQL 
 const buttonGraphQL = document.getElementById('apiError');
-buttonPost.addEventListener('click', () => {
+buttonGraphQL.addEventListener('click', () => {
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api-call/400?parm1=foo&parm2=bar');
+    xhr.open('POST', '/graphql');
     xhr.setRequestHeader('test', 'fix test2 my test');
     xhr.setRequestHeader('content-type', 'application/json'); // Use JSON content type for GraphQL
 

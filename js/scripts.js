702c8dcfa968e44a244569a3d172489c1d1a68e6
jelders25 <jack.elders@contentsquare.com>
2023-08-03 11:53:46 -0700

    change

--- js/scripts.js
+++ js/scripts.js
@@ -52,36 +52,10 @@ buttonQL.addEventListener('click', () => {
         parm1: 'foo',
         parm2: 'bar'
     });
-    const url = '/api-call/400?' + params.toString();
+    const url = '/api-call/200?' + params.toString();  // Changed the endpoint
 
     xhr.open('GET', url);
     xhr.setRequestHeader("test", "fix test2 my test");
-    xhr.setRequestHeader("content-type", "application/json"); // Use JSON content type for the response
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const simulatedResponse = JSON.stringify({
-                    error: {
-                        code: 'SIMULATED_ERROR_CODE',
-                        message: 'A simulated error object in the response body',
-                    },
-                });
-
-                const response = new Response(simulatedResponse, {
-                    status: 200,
-                    headers: { "content-type": "application/json" },
-                });
-
-                return response.json().then((data) => {
-                    console.log('Simulated 200 Response with Error:', data);
-                });
-            } else {
-                console.error('Request failed:', xhr.status, xhr.statusText);
-            }
-        }
-    };
-
-    // Send the request
-    xhr.send();
+    xhr.setRequestHeader("content-type", "text/html");
+    xhr.send()
 });

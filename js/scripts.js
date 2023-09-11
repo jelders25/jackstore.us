cf05cb4193d13bdd03517209d99a2e638252717a
jelders25 <jack.elders@contentsquare.com>
2023-09-11 11:50:28 -0700

    checkout added

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,34 @@ function btnChange() {
 function cartChange() {
     document.getElementById("labelChange").innerHTML = "Checkout";
     document.getElementById("valueChange").innerHTML = "1";
+
+    var button = document.getElementById("linkChange");
+    button.onclick = function(event) {
+        event.preventDefault();
+        window.location.href = "/checkout.html";
 }
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    window.addEventListener('load', function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation')
+  
+      // Loop over them and prevent submission
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+    }, false)
+  }())
 
 //customerror alert
 document.getElementById("customError").addEventListener("click", function () {
